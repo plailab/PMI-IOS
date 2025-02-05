cc3b9ad22acb50d056bca0100cf576e9abcf17f3
xavieryn <xavier.nishikawa@gmail.com>
2025-02-05 10:53:34 -0500

    Not Completed 3d body pose

--- PLAILabIOS/BodyCoordinator.swift
+++ PLAILabIOS/BodyCoordinator.swift
@@ -1,6 +1,6 @@
-// Coordinator.swift
 import AVFoundation
 import Vision
+import UIKit
 
 class BodyCoordinator: NSObject, AVCaptureVideoDataOutputSampleBufferDelegate {
     var parent: ScannerBodyView
@@ -18,7 +18,7 @@ class BodyCoordinator: NSObject, AVCaptureVideoDataOutputSampleBufferDelegate {
     }
     
     func detectBodyPose(in pixelBuffer: CVPixelBuffer) {
-        let request = VNDetectHumanBodyPose3DRequest{ [weak self] (request, error) in
+        let request = VNDetectHumanBodyPose3DRequest { [weak self] (request, error) in
             guard let self = self,
                   let observations = request.results as? [VNHumanBodyPose3DObservation],
                   !observations.isEmpty else {
@@ -28,47 +28,31 @@ class BodyCoordinator: NSObject, AVCaptureVideoDataOutputSampleBufferDelegate {
                 }
                 return
             }
+
             
-            if let observation = observations.first {
-                var points: [CGPoint] = []
-                let bodyJoints: [VNHumanBodyPose3DObservation.JointName] = [
-                    .topHead, .centerHead, .centerShoulder, .leftShoulder, .rightShoulder,
-                    .leftHip, .rightHip, .leftKnee, .rightKnee, .leftAnkle, .rightAnkle,
-                    .leftWrist, .rightWrist, .leftElbow, .rightElbow, .root, .spine
-                        
-                ]
-                
-                for joint in bodyJoints {
-                    let recognizedPoint = observation.recognizedPoint(joint)
-//                    if let recognizedPoint = try? observation.recognizedPoint(joint),
-//                       recognizedPoint.confidence > 0.5 {
-                    points.append(recognizedPoint.localPosition)
-                    //}
-                }
-                
-                DispatchQueue.main.async {
-                    // Only convert points if we have a valid preview layer
-                    if let previewLayer = self.previewLayer {
-                        self.parent.bodyPoints = points.map { self.convertVisionPoint($0, previewLayer: previewLayer) }
-                        self.parent.bodyPoseInfo = "Body detected with \(points.count) points"
-                    }
-                }
-            }
         }
-        
-        request.maximumHandCount = 1
         
         let handler = VNImageRequestHandler(cvPixelBuffer: pixelBuffer, orientation: .up, options: [:])
         do {
             try handler.perform([request])
         } catch {
-            print("Hand pose detection failed: \(error)")
+            print("Body pose detection failed: \(error)")
         }
     }
     
+    /// Converts a 3D body joint position to 2D screen coordinates
+    func project3DPointToScreen(_ point3D: simd_float3) -> CGPoint {
+        let x = CGFloat(point3D.x)
+        let y = CGFloat(point3D.y)
+        let screenSize = UIScreen.main.bounds.size
+        // Normalize: Vision outputs values in world space (-1 to 1), so map it to screen coordinates
+        let screenX = (x + 1) / 2 * screenSize.width
+        let screenY = (1 - y) / 2 * screenSize.height
+
+        return CGPoint(x: screenX, y: screenY)
+    }
+
     func convertVisionPoint(_ point: CGPoint, previewLayer: AVCaptureVideoPreviewLayer) -> CGPoint {
-        // Convert normalized point from Vision (0,0 to 1,1) to AVCaptureVideoPreviewLayer coordinates
-        let pointInLayer = previewLayer.layerPointConverted(fromCaptureDevicePoint: point)
-        return pointInLayer
+        return previewLayer.layerPointConverted(fromCaptureDevicePoint: point)
     }
 }

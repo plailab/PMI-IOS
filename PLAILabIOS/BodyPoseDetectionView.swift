99d12d70112e3589365df1544cee078c3e68a7f2
xavieryn <xavier.nishikawa@gmail.com>
2025-02-09 12:14:29 -0500

    Adds Body pose(grabbed from another repo) (https://github.com/philippgehrke/SquatCounter/tree/main) The body pose is currently funky because if one of the joints aren't in view, it will say that it is in the top right of the screen (assuming that is the origin). Need to update it.

--- PLAILabIOS/BodyPoseDetectionView.swift
+++ PLAILabIOS/BodyPoseDetectionView.swift
@@ -1,47 +1,28 @@
-// HandPoseDetectionView.swift
 import SwiftUI
-import AVFoundation
-import Vision
 
 struct BodyPoseDetectionView: View {
-    @Environment(\.presentationMode) var presentationMode
-    @State private var bodyPoseInfo: String = "Detecting body poses..."
-    @State private var bodyPoints: [CGPoint] = []
+    
+    @StateObject var poseEstimator = PoseEstimator()
     
     var body: some View {
-        ZStack(alignment: .bottom) {
-            BodyScannerView(bodyPoseInfo: $bodyPoseInfo, bodyPoints: $bodyPoints)
-            
-            // Draw lines between finger joints and the wrist
-            BodyLinesView(bodyPoints: bodyPoints)
-            
-            // Draw circles for the hand points
-            BodyPointsView(bodyPoints: bodyPoints)
-            
-            VStack { // vertical stack
-                // Back button at the top
-                HStack { // horizontal stack
-                    Button(action: {
-                        presentationMode.wrappedValue.dismiss()
-                    }) {
-                        Image(systemName: "xmark.circle.fill")
-                            .font(.title)
-                            .foregroundColor(.white)
-                            .padding(.top, 20)
-                    }
-                    Spacer()
-                }.padding([.top, .leading], 20) // leading is left side, trailing is right side
-                
-                Spacer()
-                
-                // Hand pose info at the bottom
-                Text(bodyPoseInfo)
-                    .padding()
-                    .background(.ultraThinMaterial)
-                    .clipShape(RoundedRectangle(cornerRadius: 10))
-                    .padding(.bottom, 50)
+        VStack {
+            ZStack {
+                GeometryReader { geo in
+                    CameraViewWrapper(poseEstimator: poseEstimator)
+                    StickFigureView(poseEstimator: poseEstimator, size: geo.size)
+                }
+            }.frame(width: UIScreen.main.bounds.size.width, height: UIScreen.main.bounds.size.width * 1920 / 1080, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+            HStack {
+                Text("Squat counter:")
+                    .font(.title)
+                Text(String(poseEstimator.squatCount))
+                    .font(.title)
+                Image(systemName: "exclamationmark.triangle.fill")
+                    .font(.largeTitle)
+                    .foregroundColor(Color.red)
+                    .opacity(poseEstimator.isGoodPosture ? 0.0 : 1.0)
             }
         }
-        .edgesIgnoringSafeArea(.all)
     }
 }
+

7b641e300e2b7d835600f716aecdc06a44aa0de0
xavieryn <xavier.nishikawa@gmail.com>
2025-02-15 09:37:45 -0500

    Small ui changes, adding the x

--- PLAILabIOS/Views/BodyPoseDetectionView.swift
+++ PLAILabIOS/Views/BodyPoseDetectionView.swift
@@ -3,15 +3,28 @@ import SwiftUI
 struct BodyPoseDetectionView: View {
     
     @StateObject var poseEstimator = PoseEstimator()
-    
+    @Environment(\.presentationMode) var presentationMode
+
     var body: some View {
         VStack {
+            HStack { // horizontal stack
+                              Button(action: {
+                                  presentationMode.wrappedValue.dismiss()
+                              }) {
+                                  Image(systemName: "xmark.circle.fill")
+                                      .font(.title)
+                                      .foregroundColor(.white)
+                                      .padding(.top, 20)
+                              }
+
+            }.padding([.top, .leading], 20) // leading is left side, trailing is right side
             ZStack {
                 GeometryReader { geo in
                     CameraViewWrapper(poseEstimator: poseEstimator)
                     StickFigureView(poseEstimator: poseEstimator, size: geo.size)
                 }
             }.frame(width: UIScreen.main.bounds.size.width, height: UIScreen.main.bounds.size.width * 1920 / 1080, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+           
             HStack {
                 Text("Squat counter:")
                     .font(.title)
@@ -21,7 +34,7 @@ struct BodyPoseDetectionView: View {
                     .font(.largeTitle)
                     .foregroundColor(Color.red)
                     .opacity(poseEstimator.isGoodPosture ? 0.0 : 1.0)
-            }
+            }.padding(.bottom, 50)
         }
     }
 }

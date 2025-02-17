e328db3043b6ee7454382df7c46dcc3f2a4fe686
welchj <weq511762338@gmail.com>
2025-02-17 15:06:48 -0500

    some more code for pacman forgot to commit in teh previous one

--- PLAILabIOS/Helper/CGPointExtension.swift
+++ PLAILabIOS/Helper/CGPointExtension.swift
@@ -1,8 +1,15 @@
 import Foundation
 import UIKit
+import CoreGraphics
 
 extension CGPoint {
     func distance(to point: CGPoint) -> CGFloat {
         return sqrt(pow(x - point.x, 2) + pow(y - point.y, 2)) // literally just the distance formula
+    }
+    static func - (lhs: CGPoint, rhs: CGPoint) -> CGPoint {
+        return CGPoint(x: lhs.x - rhs.x, y: lhs.y - rhs.y)
+    }
+    static func + (lhs: CGPoint, rhs: CGPoint) -> CGPoint {
+        return CGPoint(x: lhs.x + rhs.x, y: lhs.y + rhs.y)
     }
 }

face7f7a8e8dd65df36667068b3b2918f7f4370f
welchj <weq511762338@gmail.com>
2025-03-03 21:31:09 -0500

    fixed backbutton not workign and initialized the strawberries int eh center for pacman

--- PLAILabIOS/Views/ShoulderRaiseGameView.swift
+++ PLAILabIOS/Views/ShoulderRaiseGameView.swift
@@ -16,111 +16,118 @@ struct ShoulderRaiseGameView: View {
     
     var body: some View {
         if poseEstimator.bodyParts.isEmpty == false {
-            ZStack {
-                // consider using a more visual element, avoiding similar color with background
-                Image("strawberry")
-                    .resizable()
-                    .scaledToFit()
-                    .frame(width: 50, height: 50)
-                    .position(dotPositionLeft)
-                Image("strawberry")
-                    .resizable()
-                    .scaledToFit()
-                    .frame(width: 50, height: 50)
-                    .position(dotPositionRight)
+            GeometryReader { geometry in
+                let screenWidth = geometry.size.width
+                let screenHeight = geometry.size.height
                 
-                // hide the pacman if it is outside of the two sides of the screen
-                if poseEstimator.bodyParts[.leftWrist]?.x ?? 0 != 0 {
-                    Image("pacman\(frameIndex)") // Switch images based on the condition
+                ZStack {
+                    // consider using a more visual element, avoiding similar color with background
+                    Image("strawberry")
                         .resizable()
                         .scaledToFit()
-                        .frame(width: 40, height: 40)
-                        .position(inversePoint(poseEstimator.bodyParts[.leftWrist]!.location, in: size))
-                }
-                
-                if poseEstimator.bodyParts[.rightWrist]?.x ?? 0 != 0 {
-                    Image("pacman\(frameIndex)") // Switch images based on the condition
+                        .frame(width: 50, height: 50)
+                        .position(dotPositionLeft)
+                    Image("strawberry")
                         .resizable()
                         .scaledToFit()
-                        .frame(width: 40, height: 40)
-                        .position(inversePoint(poseEstimator.bodyParts[.rightWrist]!.location, in: size))
-                }
-            }
-            .onAppear {
-                // Start the timer when the view appears
-                Timer.scheduledTimer(withTimeInterval: 0.2, repeats: true) { _ in
-                    // Loop through frames
-                    frameIndex = (frameIndex % frameCount) + 1
-                }
-            }
-            .onChange(of: poseEstimator.bodyParts) { _ in
-                // Update points whenever the bodyParts change
-                
-                if let leftShoulder = poseEstimator.bodyParts[.leftShoulder]?.location,
-                   let rightShoulder = poseEstimator.bodyParts[.rightShoulder]?.location {
+                        .frame(width: 50, height: 50)
+                        .position(dotPositionRight)
                     
-                    // Only proceed if both leftWrist and rightWrist are available
-                    if let leftWrist = poseEstimator.bodyParts[.leftWrist]?.location,
-                       let rightWrist = poseEstimator.bodyParts[.rightWrist]?.location {
+                    // hide the pacman if it is outside of the two sides of the screen
+                    if poseEstimator.bodyParts[.leftWrist]?.x ?? 0 != 0 {
+                        Image("pacman\(frameIndex)") // Switch images based on the condition
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: 40, height: 40)
+                            .position(inversePoint(poseEstimator.bodyParts[.leftWrist]!.location, in: size))
+                    }
+                    
+                    if poseEstimator.bodyParts[.rightWrist]?.x ?? 0 != 0 {
+                        Image("pacman\(frameIndex)") // Switch images based on the condition
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: 40, height: 40)
+                            .position(inversePoint(poseEstimator.bodyParts[.rightWrist]!.location, in: size))
+                    }
+                }
+                .onAppear {
+                    dotPositionLeft = CGPoint(x: screenWidth * 0.40, y: screenHeight * 0.5)
+                    dotPositionRight = CGPoint(x: screenWidth * 0.60, y: screenHeight * 0.5)
+                    // Start the timer when the view appears
+                    Timer.scheduledTimer(withTimeInterval: 0.2, repeats: true) { _ in
+                        // Loop through frames
+                        frameIndex = (frameIndex % frameCount) + 1
+                    }
+                }
+                .onChange(of: poseEstimator.bodyParts) { _ in
+                    // Update points whenever the bodyParts change
+                    
+                    if let leftShoulder = poseEstimator.bodyParts[.leftShoulder]?.location,
+                       let rightShoulder = poseEstimator.bodyParts[.rightShoulder]?.location {
                         
-                        // Perform inverse calculation on both wrists and shoulders
-                        let inverseLeftShoulder = inversePoint(leftShoulder, in: size)
-                        let inverseRightShoulder = inversePoint(rightShoulder, in: size)
-                        let inverseLeftWrist = inversePoint(leftWrist, in: size)
-                        let inverseRightWrist = inversePoint(rightWrist, in: size)
-                        
-                        // calculating the arm lengths and dynamically updating the lengths for dots' y offset
-                        if let leftElbow = poseEstimator.bodyParts[.leftElbow]?.location,
-                           let rightElbow = poseEstimator.bodyParts[.rightElbow]?.location {
+                        // Only proceed if both leftWrist and rightWrist are available
+                        if let leftWrist = poseEstimator.bodyParts[.leftWrist]?.location,
+                           let rightWrist = poseEstimator.bodyParts[.rightWrist]?.location {
                             
-                            // Inverse the elbow points first
-                            let invLeftElbow = inversePoint(leftElbow, in: size)
-                            let invRightElbow = inversePoint(rightElbow, in: size)
+                            // Perform inverse calculation on both wrists and shoulders
+                            let inverseLeftShoulder = inversePoint(leftShoulder, in: size)
+                            let inverseRightShoulder = inversePoint(rightShoulder, in: size)
+                            let inverseLeftWrist = inversePoint(leftWrist, in: size)
+                            let inverseRightWrist = inversePoint(rightWrist, in: size)
                             
-                            // Compute distances using inverted elbow points
-                            let leftTotalArm = inverseLeftShoulder.distance(to: invLeftElbow) + invLeftElbow.distance(to: inverseLeftWrist)
-                            let rightTotalArm = inverseRightShoulder.distance(to: invRightElbow) + invRightElbow.distance(to: inverseRightWrist)
-                            
-                            // Update arm length with the max inverted distance
-                            armLength = max(leftTotalArm, rightTotalArm)
-                            //                            print(armLength)
-                        }
-                        
-                        
-                        // Check if the dots are initially (0,0) and set them to the top position
-                        if dotPositionLeft == .zero && dotPositionRight == .zero {
-                            
-                            // Initial position
-                            dotPositionLeft = (inverseLeftShoulder - CGPoint(x: 0, y: dotOffsetY)).bounded(to: size)
-                            dotPositionRight = (inverseRightShoulder - CGPoint(x: 0, y: dotOffsetY)).bounded(to: size)
-                            
-                            showDotTop = true // Start at the top
-                        }
-                        
-                        if showDotTop {
-                            // Check if both wrists are close enough to their respective "top" positions
-                            if dotPositionLeft.distance(to: inverseLeftWrist) <= collisionTolerance &&
-                                dotPositionRight.distance(to: inverseRightWrist) <= collisionTolerance {
+                            // calculating the arm lengths and dynamically updating the lengths for dots' y offset
+                            if let leftElbow = poseEstimator.bodyParts[.leftElbow]?.location,
+                               let rightElbow = poseEstimator.bodyParts[.rightElbow]?.location {
                                 
-                                showDotTop.toggle()
+                                // Inverse the elbow points first
+                                let invLeftElbow = inversePoint(leftElbow, in: size)
+                                let invRightElbow = inversePoint(rightElbow, in: size)
                                 
-                                // Move dots to the bottom position
-                                dotPositionLeft = (inverseLeftShoulder + CGPoint(x: -armLength/2, y: 40)).bounded(to: size)
-                                dotPositionRight = (inverseRightShoulder + CGPoint(x: armLength/2, y: 40)).bounded(to: size)
+                                // Compute distances using inverted elbow points
+                                let leftTotalArm = inverseLeftShoulder.distance(to: invLeftElbow) + invLeftElbow.distance(to: inverseLeftWrist)
+                                let rightTotalArm = inverseRightShoulder.distance(to: invRightElbow) + invRightElbow.distance(to: inverseRightWrist)
+                                
+                                // Update arm length with the max inverted distance
+                                armLength = max(leftTotalArm, rightTotalArm)
+                                //                            print(armLength)
                             }
-                        } else {
-                            // Check if both wrists are touching their respective "bottom" positions
-                            if dotPositionLeft.distance(to: inverseLeftWrist) <= collisionTolerance &&
-                                dotPositionRight.distance(to: inverseRightWrist) <= collisionTolerance {
+                            
+                            
+                            // Check if the dots are initially (0,0) and set them to the top position
+                            if dotPositionLeft == .zero && dotPositionRight == .zero {
                                 
-                                showDotTop.toggle()
+                                // Initial position
+                                dotPositionLeft = (inverseLeftShoulder - CGPoint(x: 0, y: dotOffsetY)).bounded(to: size)
+                                dotPositionRight = (inverseRightShoulder - CGPoint(x: 0, y: dotOffsetY)).bounded(to: size)
                                 
-                                // Move dots to the top position
-                                dotPositionLeft = (inverseLeftShoulder - CGPoint(x: 0, y: armLength)).bounded(to: size)
-                                dotPositionRight = (inverseRightShoulder - CGPoint(x: 0, y: armLength)).bounded(to: size)
+                                showDotTop = true // Start at the top
                             }
+                            
+                            if showDotTop {
+                                // Check if both wrists are close enough to their respective "top" positions
+                                if dotPositionLeft.distance(to: inverseLeftWrist) <= collisionTolerance &&
+                                    dotPositionRight.distance(to: inverseRightWrist) <= collisionTolerance {
+                                    
+                                    showDotTop.toggle()
+                                    
+                                    // Move dots to the bottom position
+                                    dotPositionLeft = (inverseLeftShoulder + CGPoint(x: -armLength/2, y: 40)).bounded(to: size)
+                                    dotPositionRight = (inverseRightShoulder + CGPoint(x: armLength/2, y: 40)).bounded(to: size)
+                                }
+                            } else {
+                                // Check if both wrists are touching their respective "bottom" positions
+                                if dotPositionLeft.distance(to: inverseLeftWrist) <= collisionTolerance &&
+                                    dotPositionRight.distance(to: inverseRightWrist) <= collisionTolerance {
+                                    
+                                    showDotTop.toggle()
+                                    
+                                    // Move dots to the top position
+                                    dotPositionLeft = (inverseLeftShoulder - CGPoint(x: 0, y: armLength)).bounded(to: size)
+                                    dotPositionRight = (inverseRightShoulder - CGPoint(x: 0, y: armLength)).bounded(to: size)
+                                }
+                            }
+                            
                         }
-                        
                     }
                 }
                 

fa8911e266a2577b85fa82a1e7b5625bd316ad23
xavieryn <xavier.nishikawa@gmail.com>
2025-03-09 17:32:43 -0400

    Adds a 60 second timer for the rest in between a set

--- PLAILabIOS/Views/BodyPoseDetectionView.swift
+++ PLAILabIOS/Views/BodyPoseDetectionView.swift
@@ -6,68 +6,112 @@ struct BodyPoseDetectionView: View {
     @Environment(\.presentationMode) var presentationMode
     @StateObject private var poseEstimator: PoseEstimator
     
-    // Seems like the constructor
-    // Makes sure that the passed in parameter gets assigned to this class
+    // Timer related properties
+    @State private var counter = 0
+    private let timerLimit = 60
+    @State private var timer: Timer? = nil
+    @State private var isTimerRunning = false
+    
+    // Constructor
     init(exercise: String) {
-        
         self.exercise = exercise
-        
-        // Create the StateObject after self.exercise is initialized
-        
-        // NEED TO PASS IN THE VALUES TO THE POSEESTIMATOR SO IT CAN ACTUALLY USE IT FOR EXERCISES
         self._poseEstimator = StateObject(wrappedValue: PoseEstimator(selectedExercise: exercise))
     }
-    
     
     var body: some View {
         VStack {
             ZStack {
                 GeometryReader { geo in
                     CameraViewWrapper(poseEstimator: poseEstimator)
-                    //                    StickFigureView(poseEstimator: poseEstimator, size: geo.size)
                     GameView(poseEstimator: poseEstimator, size: geo.size, exercise: exercise)
                 }
-            }.edgesIgnoringSafeArea(.all)
-                .frame(width: UIScreen.main.bounds.size.width, height: UIScreen.main.bounds.size.width * 1920 / 1080, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
-            // make back button more obvious
-            //            .navigationBarBackButtonHidden(true) // Hide default back button
-                .navigationBarBackButtonHidden(true) // Hide default back button
-                .toolbar {
-                    ToolbarItem(placement: .navigationBarLeading) {
-                        Button(action: {
-                            presentationMode.wrappedValue.dismiss() // Navigate back
-                        }) {
-                            HStack {
-                                Image(systemName: "chevron.left")
-                                    .font(.system(size: 18, weight: .bold))
-                                Text("Back")
-                                    .font(.system(size: 18))
-                            }
-                            .foregroundColor(.white)
-                            .padding(5)
-                            .background(Color.blue) // Background color
-                            .cornerRadius(8)
+            }
+            .edgesIgnoringSafeArea(.all)
+            .frame(width: UIScreen.main.bounds.size.width,
+                   height: UIScreen.main.bounds.size.width * 1920 / 1080,
+                   alignment: .center)
+            .navigationBarBackButtonHidden(true)
+            .toolbar {
+                ToolbarItem(placement: .navigationBarLeading) {
+                    Button(action: {
+                        presentationMode.wrappedValue.dismiss()
+                    }) {
+                        HStack {
+                            Image(systemName: "chevron.left")
+                                .font(.system(size: 18, weight: .bold))
+                            Text("Back")
+                                .font(.system(size: 18))
                         }
+                        .foregroundColor(.white)
+                        .padding(5)
+                        .background(Color.blue)
+                        .cornerRadius(8)
                     }
                 }
+            }
             
-            
+            // Exercise count display
             HStack {
                 Text("\(exercise) :")
                     .font(.title)
-                Text(String(poseEstimator.exerciseCount)) // This needs to be changed to be dependent on the exercise
-                // maybe i should have a struct so that if they choose the shoulder raise variable, there is a thing for the count and that stuff automatically
+                Text(String(poseEstimator.exerciseCount))
                     .font(.title)
+            }
+            
+            // Timer display
+            HStack {
+                Text("Timer: ")
+                    .font(.title2)
+                Text("\(timerLimit - counter)") // for a 30 second break
+                    .font(.title2)
+            }
+            
+            // Timer controls
+            HStack(spacing: 20) {
+                Button(action: startTimer) {
+                    Text("Start Timer")
+                        .padding()
+                        .background(Color.green)
+                        .foregroundColor(.white)
+                        .cornerRadius(8)
+                }
                 
-            }.padding(.bottom, 50)
+                Button(action: stopTimer) {
+                    Text("Stop Timer")
+                        .padding()
+                        .background(Color.red)
+                        .foregroundColor(.white)
+                        .cornerRadius(8)
+                }
+            }
+            .padding(.bottom, 50)
         }
         .onAppear {
             UIApplication.shared.isIdleTimerDisabled = true
         }
         .onDisappear {
+            stopTimer() // Make sure to stop timer when view disappears
             UIApplication.shared.isIdleTimerDisabled = false
         }
     }
     
+    // Timer functions
+    private func startTimer() {
+        stopTimer() // Invalidate any existing timer
+        
+        isTimerRunning = true
+        timer = Timer.scheduledTimer(withTimeInterval: 1, repeats: true) { _ in
+            self.counter += 1
+        }
+        // Check if counter has reached the limit
+        if self.counter >= self.timerLimit {
+            self.stopTimer()
+        }
+    }
+    
+    private func stopTimer() {
+        timer?.invalidate()
+        timer = nil
+        isTimerRunning = false
+    }
 }
-
